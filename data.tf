635cac5e442220af8d58d1e5d94e2c984e06275b
sutarym <123245418+sutarym@users.noreply.github.com>
2023-05-12 15:40:05 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,16 +1,2 @@
-terraform {
-  backend "s3" {
-    bucket = "yasin-sutar-bucket"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
-    access_key = "AKIAXQM6UOUEN4AYOC5O"
-  secret_key = "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"
-  }
-}
 
-
-provider "aws" {
-  region     = "ap-south-1"
-  
-}
 
