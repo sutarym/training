52997a8d59bd924b3eb39ea8dae1bd58db876736
sutarym <123245418+sutarym@users.noreply.github.com>
2023-05-12 15:39:49 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,13 +1,16 @@
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = "AKIAXQM6UOUEN4AYOC5O"
-  secret_key = "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"
-}
-
 terraform {
   backend "s3" {
     bucket = "yasin-sutar-bucket"
     key    = "terraform.tfstate"
     region = "ap-south-1"
+    access_key = "AKIAXQM6UOUEN4AYOC5O"
+  secret_key = "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"
   }
 }
+
+
+provider "aws" {
+  region     = "ap-south-1"
+  
+}
+
