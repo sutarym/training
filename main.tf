df3a7ca91c42072e6226e9f9e7b0133b4af8eb23
sutarym <123245418+sutarym@users.noreply.github.com>
2023-05-12 15:28:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAXQM6UOUEOX2EPYH5"
-  secret_key = "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"
+  #access_key = "AKIAXQM6UOUEOX2EPYH5"
+  #secret_key = "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"
 }
