6ef8b876a1a9e0049cff331879dbcaca8cd59c89
sutarym <123245418+sutarym@users.noreply.github.com>
2023-05-12 15:43:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,3 +3,12 @@ provider "aws" {
 }
 
 # Add your S3 backend configuration here
+
+
+  backend "s3" {
+    bucket = "yasin-sutar-bucket"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+    access_key = "AKIAXQM6UOUEN4AYOC5O"
+  secret_key = "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"
+  }
