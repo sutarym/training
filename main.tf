88675798fcb1989f9b19e4bc13267239e94361f0
sutarym <123245418+sutarym@users.noreply.github.com>
2023-05-12 15:40:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "yasin-sutar-bucket"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+    access_key = "AKIAXQM6UOUEN4AYOC5O"
+  secret_key = "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"
+  }
+}
+
 
 provider "aws" {
-  region = "ap-south-1"
-  #access_key = "AKIAXQM6UOUEOX2EPYH5"
-  #secret_key = "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"
+  region     = "ap-south-1"
+  
 }
