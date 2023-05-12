553fe13e1163b8c9a16578deb1e0090655aaab1e
sutarym <123245418+sutarym@users.noreply.github.com>
2023-05-12 16:02:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,1 @@
-provider "aws" {
-  region  = "ap-south-1" # Don't change the region
-   access_key = "AKIAXQM6UOUEN4AYOC5O"
-  secret_key = "TxOBwWWdcW7dvRVzZ3AimIKIuFABMRZxR5IEt445"
-}
 
-# Add your S3 backend configuration here
-
-terraform{
-  backend "s3" {
-    bucket = "yasin-sutar-bucket"
-    key    = "Yasin.Sutar"
-    region = "ap-south-1"
-   
-  }
-}
