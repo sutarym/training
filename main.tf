87843727f675ec3d6323b27693a198c289eb7947
sutarym <123245418+sutarym@users.noreply.github.com>
2023-05-12 15:15:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,1 @@
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = "AKIAXQM6UOUEN4AYOC5O"
-  secret_key = "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"
-}
 
-resource "aws_instance" "web" {
-  ami           = "ami-06c2ec1ceac22e8d6"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
