da73301736ace4d407b8937b8f08663b76843497
sutarym <123245418+sutarym@users.noreply.github.com>
2023-05-15 12:36:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@
 
 provider "aws" {
   region = "ap-south-1"
+  access_key = "AKIAXQM6UOUEN4AYOC5O"
+secret_key = "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"
+
 }
 resource "aws_instance" "myFirstInstance" {
   ami           = "ami-06c2ec1ceac22e8d6"
