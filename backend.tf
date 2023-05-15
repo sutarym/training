2891db483aecfeedc9760c57c7eacfa9e916d646
sutarym <123245418+sutarym@users.noreply.github.com>
2023-05-15 17:22:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "delete-bucket-yasint"
     key    = "terraform.tfstate"
     region = "ap-south-1"
-    
+      access_key = "AKIAXQM6UOUEIV6HREG5"
+secret_key = "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"
   }
 }
