7ac42acfdb19cb36bbf20a5b6dd770948b3d0549
sutarym <123245418+sutarym@users.noreply.github.com>
2023-05-15 15:16:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@
 
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAXQM6UOUEN4AYOC5O"
-secret_key = "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"
+  access_key = "AKIAXQM6UOUEC6CB7O4I"
+secret_key = "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"
 
 }
 
@@ -16,5 +16,6 @@ resource "aws_instance" "myFirstInstance" {
   ami           = "ami-06c2ec1ceac22e8d6"
   key_name = "yasin-sutar"
   instance_type = "t2.micro"
+  subnet_id     = "subnet-0ebba453411cf5f33"
   
 }
