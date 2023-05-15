5d05b41a0b2931a58a2ac5fe5ff84fb6ca780a78
sutarym <123245418+sutarym@users.noreply.github.com>
2023-05-15 12:12:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 
 
 
-
+provider "aws" {
+  region = "ap-south-1"
+}
 resource "aws_instance" "myFirstInstance" {
   ami           = "ami-06c2ec1ceac22e8d6"
-#  key_name = var.key_name
+  key_name = "yasin-sutar"
   instance_type = "t2.micro"
   
 }
