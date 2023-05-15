33dffc32d54c8145c84533344bbf54243b3e1f6a
sutarym <123245418+sutarym@users.noreply.github.com>
2023-05-15 13:04:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,36 +8,15 @@ secret_key = "TxOBwWWdcW7dvRVzZ3AimIKIuFABMRZxR5IEt445"
 
 }
 
-
-
-
-
-resource "aws_s3_bucket" "terraform_state_for_yasin_ankal" {
-  bucket = "terraform-state"
+backend "s3" {
+  bucket = "yasin-sutar-bucket"
   region = "ap-south-1"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Stmt1",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::516265637128:user/Sample"
-      },
-      "Action": "s3:*",
-      "Resource": "arn:aws:s3:::terraform-state/*"
-    }
-  ]
-}
-EOF
 }
 
-resource "aws_s3_bucket_object" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state_for_yasin_ankal.bucket
-  key = "terraform.tfstate"
-  source = "terraform.tfstate"
-}
+
+
+
+
 resource "aws_instance" "myFirstInstance" {
   ami           = "ami-06c2ec1ceac22e8d6"
   key_name = "yasin-sutar"
